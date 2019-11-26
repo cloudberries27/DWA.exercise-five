219a77d06f59e9dd0f42638c2316b0bf16134bb5
cloudberries27 <ccrod2298@gmail.com>
2019-11-26 15:42:34 -0500

    router edits

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Router, BrowserRouter as Router} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
 import Login from "./pages/Login";

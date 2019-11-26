6ad80381e9dff3c1eab1f7eefc7e9c243d8f6121
cloudberries27 <ccrod2298@gmail.com>
2019-11-26 16:10:19 -0500

    added universal header

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
+import Header from "./components/Header/index"
 import Login from "./pages/Login";
 import UserProfile from "./pages/UserProfile";
 import SignUp from "./pages/SignUp";
@@ -10,11 +11,11 @@ import SignUp from "./pages/SignUp";
 function App() {
   return (
     <div className="App">
+      <Header />
       <Router>
         <Route exact path='/' component={UserProfile}/>
         <Route exact path='/Login' component={Login}/>
         <Route exact path='/sign-uo' component={SignUp}/>
-
       </Router>
     </div>
   );
---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Header(){
+    return(
+        <header>
+            <nav>
+                <a href='/'>Home</a>
+                <a href='/login'>Login</a>
+                <a href='/sign-uo'>Sign Up</a>
+                <a href='/log-out'>Log Out</a>
+            </nav>
+        </header>
+    )
+}
